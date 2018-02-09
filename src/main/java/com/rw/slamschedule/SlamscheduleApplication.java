85e3ff9a4c7b3b414496aed6bc3e230fbc1229c5
朱熙 <zhuxi@zhuxideMacBook-Pro.local>
2018-02-09 08:21:01 +0800

    Initial commit

---
+++ src/main/java/com/rw/slamschedule/SlamscheduleApplication.java
@@ -0,0 +1,12 @@
+package com.rw.slamschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlamscheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlamscheduleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/rw/slamschedule/SlamscheduleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rw.slamschedule;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SlamscheduleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
